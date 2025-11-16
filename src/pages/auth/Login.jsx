3a9a0d9551f0fe5053906e9c023e78041c35a43c
Moyosore1605 <moyosorejesuojo@gmail.com>
2025-11-16 15:22:12 +0100

    Added basic function for login and signup

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-import { useEffect } from 'react'
-import api from '../../api/axios.js'
+import api from '../../api/axios.js';
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-    return (
-        <div>Login</div>
-    )
+    const { login } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        const res = await login({ email, password });
+        if (!res.success) {
+        setError(res.message);
+        return;
+        }
+        navigate("/dashboard");
+    };
+
+return (
+    <form onSubmit={handleLogin} className='m-10 flex gap-3'>
+        <input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} className='border p-5'/>
+        <input  placeholder="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} className='border p-5'/>
+        <button>Login</button>
+        {error && <p>{error}</p>}
+    </form>
+);
 }
--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function Signup() {
-    return (
-        <div>signup</div>
-    )
+    const { register } = useContext(AuthContext);
+    const [form, setForm] = useState({name: "", email: "",  password: "",});
+    const [error, setError] = useState("");
+    const [done, setDone] = useState(false);
+
+    const handleSignup = async (e) => {
+        e.preventDefault();
+        const res = await register(form);
+        if (!res.success) {
+            setError(res.message);
+            return;
+        }
+        setDone(true);
+    };
+
+return (
+    <form onSubmit={handleSignup} className="m-10 flex gap-3">
+        <input  placeholder="name" value={form.name} onChange={(e) => setForm({ ...form, name: e.target.value })} className="border p-5"/>
+        <input placeholder="email" value={form.email} onChange={(e) => setForm({ ...form, email: e.target.value })} className="border p-5"/>
+        <input placeholder="password" type="password" value={form.password} onChange={(e) => setForm({ ...form, password: e.target.value })} className="border p-5"/>
+        <button>Sign Up</button>
+
+        {error && <p>{error}</p>}
+        {done && <p>Signup successful!</p>}
+    </form>
+);
 }
+
